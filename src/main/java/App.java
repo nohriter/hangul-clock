53efe9cef8850375128039038493d474f3e46712
nohriter <nohriter@gmail.com>
2021-11-17 03:13:01 +0900

    구현완료

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,6 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 public class App {
     public static void main(String[] args) {
-
-
-
+        Clock clock = new Clock();
+        clock.print();
     }
 }
--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -46,50 +46,115 @@ public class Clock {
     }
 
     private void changeMinute() {
+        String[] minutes = this.minute.split("");
+        String firstMinute = minutes[0];
+        String lastMinute = minutes[1];
+
+        //기본값 십
+        hangulClock[3][5] = ANSI_RED + hangulClock[3][5] + ANSI_RESET;
+
+        switch (firstMinute) {
+            case "0" :
+                hangulClock[3][5] = "십";
+                break;
+            case "2" :
+                hangulClock[3][1] = ANSI_RED + hangulClock[3][1] + ANSI_RESET;
+                break;
+            case "3" :
+                hangulClock[3][2] = ANSI_RED + hangulClock[3][2] + ANSI_RESET;
+                break;
+            case "4" :
+                hangulClock[3][3] = ANSI_RED + hangulClock[3][3] + ANSI_RESET;
+                break;
+            case "5" :
+                hangulClock[3][4] = ANSI_RED + hangulClock[3][4] + ANSI_RESET;
+                break;
+        }
+
+        //기본값 분
+        hangulClock[5][5] = ANSI_RED + hangulClock[5][5] + ANSI_RESET;
+
+        switch (lastMinute) {
+            case "1" :
+                hangulClock[4][1] = ANSI_RED + hangulClock[4][1] + ANSI_RESET;
+                break;
+            case "2" :
+                hangulClock[4][2] = ANSI_RED + hangulClock[4][2] + ANSI_RESET;
+                break;
+            case "3" :
+                hangulClock[4][3] = ANSI_RED + hangulClock[4][3] + ANSI_RESET;
+                break;
+            case "4" :
+                hangulClock[4][4] = ANSI_RED + hangulClock[4][4] + ANSI_RESET;
+                break;
+            case "5" :
+                hangulClock[5][1] = ANSI_RED + hangulClock[5][1] + ANSI_RESET;
+                break;
+            case "6" :
+                hangulClock[4][5] = ANSI_RED + hangulClock[4][5] + ANSI_RESET;
+                break;
+            case "7" :
+                hangulClock[5][2] = ANSI_RED + hangulClock[5][2] + ANSI_RESET;
+                break;
+            case "8" :
+                hangulClock[5][3] = ANSI_RED + hangulClock[5][3] + ANSI_RESET;
+                break;
+            case "9" :
+                hangulClock[5][4] = ANSI_RED + hangulClock[5][4] + ANSI_RESET;
+                break;
+        }
 
     }
 
     private void changeHour() {
-//        Scanner sc = new Scanner(System.in);
-//        hour = sc.next();
-        System.out.println(this.hour);
         switch (this.hour) {
             case "01" :
+            case "13" :
                 hangulClock[0][0] = ANSI_RED + hangulClock[0][0] + ANSI_RESET;
                 break;
             case "02" :
+            case "14" :
                 hangulClock[0][1] = ANSI_RED + hangulClock[0][1] + ANSI_RESET;
                 break;
             case "03" :
+            case "15" :
                 hangulClock[0][2] = ANSI_RED + hangulClock[0][2] + ANSI_RESET;
                 break;
             case "04" :
+            case "16" :
                 hangulClock[0][3] = ANSI_RED + hangulClock[0][3] + ANSI_RESET;
                 break;
             case "05" :
+            case "17" :
                 hangulClock[0][4] = ANSI_RED + hangulClock[0][4] + ANSI_RESET;
                 hangulClock[0][5] = ANSI_RED + hangulClock[0][5] + ANSI_RESET;
                 break;
             case "06" :
+            case "18" :
                 hangulClock[1][0] = ANSI_RED + hangulClock[1][0] + ANSI_RESET;
                 hangulClock[1][1] = ANSI_RED + hangulClock[1][1] + ANSI_RESET;
                 break;
             case "07" :
+            case "19" :
                 hangulClock[1][2] = ANSI_RED + hangulClock[1][2] + ANSI_RESET;
                 hangulClock[1][3] = ANSI_RED + hangulClock[1][3] + ANSI_RESET;
                 break;
             case "08" :
+            case "20" :
                 hangulClock[1][4] = ANSI_RED + hangulClock[1][4] + ANSI_RESET;
                 hangulClock[1][5] = ANSI_RED + hangulClock[1][5] + ANSI_RESET;
                 break;
             case "09" :
+            case "21" :
                 hangulClock[2][0] = ANSI_RED + hangulClock[2][0] + ANSI_RESET;
                 hangulClock[2][1] = ANSI_RED + hangulClock[2][1] + ANSI_RESET;
                 break;
             case "10" :
+            case "22" :
                 hangulClock[2][2] = ANSI_RED + hangulClock[2][2] + ANSI_RESET;
                 break;
             case "11" :
+            case "23" :
                 hangulClock[2][2] = ANSI_RED + hangulClock[2][2] + ANSI_RESET;
                 hangulClock[2][3] = ANSI_RED + hangulClock[2][3] + ANSI_RESET;
                 break;
@@ -110,8 +175,4 @@ public class Clock {
         hangulClock[2][5] = ANSI_RED + hangulClock[2][5] + ANSI_RESET;
     }
 
-    public static void main(String[] args) {
-        Clock clock = new Clock();
-        clock.print();
-    }
 }
